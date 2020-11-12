948dc6cbf0b3ede0724b8725b9bac9e25712fe26
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-12 14:45:05 +0100

    add upper and lower date ranges

--- scripts/json-to-xml.py
+++ scripts/json-to-xml.py
@@ -1,9 +1,12 @@
-from SariDateParser.dateParser import parse
+from edtf import parse_edtf
+from edtf.parser.parser_classes import Interval, Date
+from sariDateParser.dateParser import parse
 from lxml import etree
 import json
 import os
 import requests
 import urllib
+import time
 
 inputFile = "/input/sari_abzug-utf-8_23_04-tsv.txt"
 manifestDirectory = "/manifests/"
@@ -11,6 +14,33 @@ outputDirectory = "/input/"
 outputPrefix = "sari-"
 
 fieldsContainingDates = ['100$d', '260$c', '260$g', '264$c', '533$d', '600$d', '611$d', '700$d']
+
+def convertEDTFdate(date):
+    try:
+        d = parse_edtf(downgradeEDTF(date))
+    except:
+        raise ValueError('Invalid date', date)
+    
+    if 'Interval' in str(type(d)):
+        if type(d.lower) is list:
+            lower = d.lower[0].lower_strict()
+        else:
+            lower = d.lower.lower_strict()
+        if type(d.upper) is list:
+            upper = d.upper[0].upper_strict()
+        else:
+            upper = d.upper.upper_strict()
+    else:
+        if type(d) is list:
+            lower = d[0].lower_strict()
+            upper = d[0].upper_strict()
+        else:
+            lower = d.lower_strict()
+            upper = d.upper_strict()
+    return {
+        'lower': time.strftime("%m/%d/%Y", lower),
+        'upper': time.strftime("%m/%d/%Y", upper)
+    }
 
 def convertRowToXml(row, keys):
     record = etree.Element("record")
@@ -27,12 +57,26 @@ def convertRowToXml(row, keys):
                     parsedDate = parse(row[key])
                     if parsedDate:
                         subfield.set("parsedDate", parsedDate)
+                        daterange = convertEDTFdate(parsedDate)
+                        subfield.set("upperDate", daterange['upper'])
+                        subfield.set("lowerdate", daterange['lower'])
                 # Remove non-separated field content
                 datafield.text = None
             else:
                 datafield = etree.SubElement(record, "datafield", tag=key)
                 datafield.text = str(row[key])
     return record
+
+def downgradeEDTF(date):
+    """
+    Convert a edtf date string to the previous version supported by the python edtf package
+    """
+    edtfDate = date.replace('X','u')
+    if edtfDate[-1:] == '/':
+        edtfDate += 'uuuu-uu'
+    if edtfDate[0] == '/':
+        edtfDate = 'uuuu-uu' + edtfDate
+    return edtfDate
 
 def getImagesFromCachedManifest(manifest):
     manifestFilePath = manifestDirectory + urllib.parse.quote(manifest, safe='') + '.json'
