d8b62ac0006c9979e2cc3cc9a027f03e1f000025
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-12 16:32:17 +0100

    add conversion to workflow

--- run.sh
+++ run.sh
@@ -5,6 +5,16 @@ X3MLCONTAINER=$(echo $PROJECT_NAME)_x3ml
 
 if [[ $NOPROMPT -ne 1 ]]
 then
+  read -p "Convert to XML? (y/n)" -n 1 -r
+  echo ""
+fi
+if [[ $NOPROMPT || $REPLY =~ ^[Yy]$ ]]
+then
+  docker exec $JOBSCONTAINER bash -c "python /scripts/json-to-xml.py"
+fi
+
+if [[ $NOPROMPT -ne 1 ]]
+then
   read -p "Perform mapping? (y/n)" -n 1 -r
   echo ""
 fi
