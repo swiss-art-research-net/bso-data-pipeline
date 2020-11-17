11efae110ccbeb1055290759e58811b7f4e95ad3
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-17 13:25:17 +0100

    include external curated fields

--- scripts/json-to-xml.py
+++ scripts/json-to-xml.py
@@ -7,12 +7,17 @@ import requests
 import urllib
 import time
 
-inputFile = "/input/sari_abzug-utf-8_23_04-tsv.json"
+inputFile = "/input/sari_abzug-utf-8_23_04-tsv-partial.json"
+externalFieldsDirectory = "../input/"
 manifestDirectory = "/manifests/"
 outputDirectory = "/input/"
 outputPrefix = "sari-"
 
+# List fields that contain dates. Those will be passed to the parser
 fieldsContainingDates = ['100$d', '260$c', '260$g', '264$c', '533$d', '600$d', '611$d', '700$d']
+
+# List fields that are loaded from separate files (e.g. curated and/or multi-value fields)
+externalFields = ['651']
 
 def convertEDTFdate(date):
     try:
@@ -41,29 +46,43 @@ def convertEDTFdate(date):
         'upper': time.strftime("%Y-%m-%d", upper)
     }
 
-def convertRowToXml(row, keys):
+def convertRowToXml(row, keys, externalFields):
     record = etree.Element("record")
-    etree.SubElement(record, "uuid").text = row['UUID']
+    etree.SubElement(record, "uuid").text = row['id']
     datafield = False
     for key in keys:
-        if key in row and row[key] is not None:
-            if '$' in key:
-                code = key[4:]
-                subfield = etree.SubElement(datafield, "subfield", code=code)
-                subfield.text = str(row[key])
-                # Check if field contains a date
-                if key in fieldsContainingDates:
-                    parsedDate = parse(row[key])
-                    if parsedDate:
-                        subfield.set("parsedDate", parsedDate)
-                        daterange = convertEDTFdate(parsedDate)
-                        subfield.set("upperDate", daterange['upper'])
-                        subfield.set("lowerDate", daterange['lower'])
-                # Remove non-separated field content
-                datafield.text = None
-            else:
-                datafield = etree.SubElement(record, "datafield", tag=key)
-                datafield.text = str(row[key])
+        # Check if key is a field that gets loaded externally (check only part before $ if present)
+        if key.split('$')[0] in externalFields.keys():
+            # Ignore the subfields as they will be loaded from the external fields
+            if not '$' in key:
+                # Select the field values based on the UUIDs
+                fieldsToInclude = [d for d in externalFields[key] if d['UUID'] == row['id']]
+                for f in fieldsToInclude:
+                    # Create a datafield for each set of values
+                    datafield = etree.SubElement(record, "datafield", tag=key)
+                    for k in [d for d in f.keys() if key in d]:
+                        code = k.split('_')[1].replace(' ','_')
+                        subfield = etree.SubElement(datafield, "subfield", code=code)
+                        subfield.text = str(f[k])
+        else:
+            if key in row and row[key] is not None:
+                if '$' in key:
+                    code = key[4:]
+                    subfield = etree.SubElement(datafield, "subfield", code=code)
+                    subfield.text = str(row[key])
+                    # Check if field contains a date
+                    if key in fieldsContainingDates:
+                        parsedDate = parse(row[key])
+                        if parsedDate:
+                            subfield.set("parsedDate", parsedDate)
+                            daterange = convertEDTFdate(parsedDate)
+                            subfield.set("upperDate", daterange['upper'])
+                            subfield.set("lowerDate", daterange['lower'])
+                    # Remove non-separated field content
+                    datafield.text = None
+                else:
+                    datafield = etree.SubElement(record, "datafield", tag=key)
+                    datafield.text = str(row[key])
     return record
 
 def downgradeEDTF(date):
@@ -104,8 +123,16 @@ def imageListToXml(images):
         etree.SubElement(imageNode, "url", type="iiif").text = image['image']
     return imagesNode
 
+# Read main data file
 with open(inputFile, 'r') as f:
     rawData = json.load(f)
+
+# Read fields from external files
+externalFieldContent = {}
+for externalField in externalFields:
+    filePath = externalFieldsDirectory + externalField + '.json'
+    with open(filePath, 'r') as f:
+         externalFieldContent[externalField] = json.load(f)['rows']
 
 keys = list(rawData['rows'][0].keys())
 keys.sort()
@@ -114,7 +141,7 @@ keys.sort()
 for i, row in enumerate(rawData['rows']):
     
     records = etree.Element("records")
-    record = convertRowToXml(row, keys)
+    record = convertRowToXml(row, keys, externalFieldContent)
     
     if row['manifest']:
         images = getImagesFromCachedManifest(row['manifest'])
@@ -127,6 +154,6 @@ for i, row in enumerate(rawData['rows']):
     
     records.append(record)
     
-    outputFile = outputDirectory + outputPrefix + row['UUID'] + ".xml"
+    outputFile = outputDirectory + outputPrefix + row['id'] + ".xml"
     with open(outputFile, 'wb') as f:
         f.write(etree.tostring(records, xml_declaration=True, encoding='UTF-8', pretty_print=True))
