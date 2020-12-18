3a0579c21a3c6443b4a74a4a7122431453982549
Florian Kräutli <florian.kraeutli@uzh.ch>
2020-12-18 14:51:15 +0100

    omit lfs file for now

--- downloadSources.sh
+++ downloadSources.sh
@@ -14,4 +14,5 @@ download () {
 download "data/zbz/curate/100_curate.json" "data/source/100.json"
 download "data/zbz/curate/264_curate.txt" "data/source/264.json"
 download "data/zbz/curate/651_curate.txt" "data/source/651.json"
-download "data/zbz/source/sari_abzug-utf-8_23_04-tsv.txt" "data/source/sari_abzug-utf-8_23_04-tsv.json"
+#  TODO: support git lfs
+# download "data/zbz/source/sari_abzug-utf-8_23_04-tsv.txt" "data/source/sari_abzug-utf-8_23_04-tsv.json"
