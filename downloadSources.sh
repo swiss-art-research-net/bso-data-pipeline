2936bbd801461d6ee6d5cd4e36deaa81a81fb70d
Florian Kräutli <florian.kraeutli@uzh.ch>
2020-12-18 22:05:34 +0100

    add lfs support

--- downloadSources.sh
+++ downloadSources.sh
@@ -14,5 +14,4 @@ download () {
 download "data/zbz/curate/100_curate.json" "data/source/100.json"
 download "data/zbz/curate/264_curate.txt" "data/source/264.json"
 download "data/zbz/curate/651_curate.txt" "data/source/651.json"
-#  TODO: support git lfs
-# download "data/zbz/source/sari_abzug-utf-8_23_04-tsv.txt" "data/source/sari_abzug-utf-8_23_04-tsv.json"
+download "data/zbz/source/sari_abzug-utf-8_23_04-tsv.txt" "data/source/sari_abzug-utf-8_23_04-tsv.json"
