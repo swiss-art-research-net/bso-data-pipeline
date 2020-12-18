7ecc7bf61d4a00d27a0a03d81474b3e2ef25753c
Florian Kräutli <florian.kraeutli@uzh.ch>
2020-12-18 14:12:47 +0100

    WIP: download sources automatically

---
+++ downloadSources.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source .env
+REPO=swiss-art-research-net/BSO
+
+
+download () {
+  remotepath=$1
+  localpath=$2
+
+  echo "Downloading $remotepath"
+  python3 scripts/getFileContentsFromGit.py $GITHUB_USERNAME $GITHUB_PERSONAL_ACCESS_TOKEN $REPO $remotepath > $localpath
+}
+
+download "data/zbz/curate/100_curate.json" "data/source/100.json"
+download "data/zbz/curate/264_curate.txt" "data/source/264.json"
+download "data/zbz/curate/651_curate.txt" "data/source/651.json"
+download "data/zbz/source/sari_abzug-utf-8_23_04-tsv.txt" "data/source/sari_abzug-utf-8_23_04-tsv.json"
---
+++ scripts/getFileContentsFromGit.py
@@ -0,0 +1,33 @@
+import base64
+import requests
+import json
+import sys
+
+# Call with arguments:
+# 1: GitHub username
+# 2: GitHub personal access token
+# 3: Repository
+# 4: path to file
+username = sys.argv[1]
+token = sys.argv[2]
+repo = sys.argv[3]
+path = sys.argv[4]
+
+folder = path.rsplit('/',1)[0]
+file = path.rsplit('/',1)[1]
+
+folderURL = 'https://api.github.com/repos/' + repo + '/contents/' + folder
+folderRequest = requests.get(folderURL, auth=(username, token))
+folderData = folderRequest.json()
+
+try:
+    requestedFile = [d for d in folderData if d['name'] == file][0]
+except:
+    sys.stderr.write("could not find file %s in path %s\n" % (file, folderURL))
+    exit()
+
+fileURL = 'https://api.github.com/repos/' + repo + '/git/blobs/' + requestedFile['sha']
+fileRequest = requests.get(fileURL, auth=(username, token))
+
+# Output to console
+print( base64.b64decode( fileRequest.json()['content'] ).decode('UTF-8', 'ignore') )
