f9774f63503b9acc1e9c7a0562c1f47a0357a054
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-02-11 14:51:56 +0100

    add error handling

--- scripts/generate-materialise-query-from-field-definitions.py
+++ scripts/generate-materialise-query-from-field-definitions.py
@@ -26,9 +26,11 @@ template = Template("""
 for field in model['fields']:
     uri = model['prefix'] + field['id']
     selectQuery = [d for d in field['queries'] if 'select' in d.keys()][0]['select']
-    matches = re.findall(r'{(.*)}$', selectQuery)
-    if matches:
-        selectQueryPart = matches[0].replace('$','?').replace('"',"'")
+    try:
+        selectQueryPart = re.findall(r'{(.*)}\s*$', selectQuery)[0]
+    except:
+        print("No query found for " + field['id'])
+    selectQueryPart = selectQueryPart.replace('$','?').replace('"',"'")
     query = template.substitute(uri=uri, query=selectQueryPart)
     output += query
 
