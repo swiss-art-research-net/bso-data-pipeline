03eaf97fae07712a210311419cf23e23088f9466
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-02-18 18:07:55 +0100

    add RS fiekd definitions

--- services/researchspace/apps/sari/src/compileFieldDefinitions.py
+++ services/researchspace/apps/sari/src/compileFieldDefinitions.py
@@ -4,8 +4,10 @@ inputFile = '../../../../researchplatform/apps/bso/src/fieldDefinitions.yml'
 outputFile = '../ldp/assets/fieldDefinitions.trig'
 
 model = generator.loadSourceFromFile(inputFile)
+model['prefix'] = 'http://rs.swissartresearch.net/instances/knowledgePatterns/'
 
 output = generator.generate(model, generator.RESEARCHSPACE)
+output = output.replace("fieldDefinitionContainer","knowledgePatternContainer")
 
 with open(outputFile, 'w') as f:
     f.write(output)
