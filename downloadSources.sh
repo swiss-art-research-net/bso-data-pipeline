63becbaba3db1c2a4c4f8c11ee63a79461b4c9a2
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-11-03 11:47:10 +0100

    change path

--- downloadSources.sh
+++ downloadSources.sh
@@ -41,7 +41,7 @@ download "data/nb/source/WMC_Records_20201201.xml" "data/source/nb-records.xml"
 download "data/nb/source/Gugelman.xml" "data/source/nb-parentrecords.xml"
 
 download "data/sff/source/SFF-Datenbank-Export.csv" "data/source/SFF-Datenbank-Export.csv"
-download "data/sff/source/Werk_Masse.csv" "data/source/sff-werk-masse.csv"
+download "data/sff/sff-werk-masse.csv" "data/source/sff-werk-masse.csv"
 download "data/sff/source/Literatur.csv" "data/source/sff-literatur.csv"
 download "data/sff/source/Literatur_index_Verknupfung.csv" "data/source/sff-literatur-links.csv"
 download "data/sff/sff-images.csv" "data/source/sff-images.csv"
