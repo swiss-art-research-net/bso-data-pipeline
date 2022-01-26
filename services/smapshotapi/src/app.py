8696e153a1d9978a0683742fbdf3d0d36615ccf1
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-01-26 17:43:45 +0100

    add comments

--- services/smapshotapi/src/app.py
+++ services/smapshotapi/src/app.py
@@ -10,9 +10,36 @@ with open('token.txt', 'r') as f:
 smapshot = SmapshotConnector(url = "https://smapshot-beta.heig-vd.ch/api/v1", token = token)
 
 def error(message):
+  """
+  Generate a JSON error object
+  """
   return {"error": message}
 
 def getDataFromSparqlUpdate(payload):
+  """
+  Extracts the data from the VALUES clause of  a SPARQL update request. All other parts of the query are ignored.
+  Consider the following query:
+  
+    PREFIX : <http://www.example.org/>
+    INSERT  {
+      :s :p :o .
+    } WHERE {
+      VALUES(?type ?image_id ?iiif_url ?region_px) {
+        ("updateImageRegion" "210758" <https://www.e-rara.ch/zuz/i3f/v20/12581613> "[35,28,2196,1445]")
+      }
+    }
+
+  Given this query the function will return the following data:   
+
+  [{
+    "type": "updateImageRegion",
+    "image_id": "210758",
+    "iiif_url": "https://www.e-rara.ch/zuz/i3f/v20/12581613",
+    "region_px": "[35,28,2196,1445]"
+  }]
+
+  Each row in the VALUES clause is returned as a JSON object in the list.
+  """
   from rdflib.plugins.sparql.parser import parseUpdate
   q = parseUpdate(payload)
   keys = {}
@@ -30,6 +57,9 @@ def getDataFromSparqlUpdate(payload):
   return data
 
 def processRequest(data):
+  """
+  Routes a request based on its type and calls the appropriate function.
+  """
   for row in data:
     if 'type' in row:
       if row['type'] == "updateImageRegion":
@@ -42,6 +72,9 @@ def processRequest(data):
       return error("No type specified")
 
 def updateImageRegion(data):
+  """
+  Updates the region of an image.
+  """
   attributes = {
     "iiif_data" : {
       "image_service3_url": data['iiif_url'],
@@ -56,6 +89,9 @@ def index():
   
 @app.route('/sparql', methods=['GET', 'POST'])
 def sparql():
+  """
+  Listens for SPARQL Update requests and processes them.
+  """
   if request.form:
     data = getDataFromSparqlUpdate(request.form['update'])
     response = processRequest(data)
