b9297e5f0ae8714008c5788ba791437b1aa1b4e8
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-08-15 13:24:22 +0200

    query only missing identifiers

--- scripts/extract-gnd-data.py
+++ scripts/extract-gnd-data.py
@@ -10,6 +10,21 @@ ttlOutput='/data/ttl/additional/gnd.ttl'
 
 additionalFiles = [] # Additional files that should be scanned can be added here
 
+# Read the output file and query for existing URIs
+existingIdentifiers = []
+if path.exists(ttlOutput):
+    gndData = rdflib.Graph()
+    gndData.load(ttlOutput, format='turtle')
+    queryResults = gndData.query("""
+    PREFIX gndo:  <https://d-nb.info/standards/elementset/gnd#>
+    SELECT DISTINCT ?gnd WHERE {
+        ?gnd a gndo:AuthorityResource .
+    }
+    """)
+    for row in queryResults:
+        existingIdentifiers.append(str(row[0]))
+
+
 # Look at all Turtle files
 inputFiles = [path.join(root, name)
              for root, dirs, files in walk(ttlFolder)
@@ -47,14 +62,17 @@ for file in tqdm(inputFiles):
 # Filter non-unique valeus
 gndIdentifiers = list(set(gndIdentifiers))
 
-# Clear output file
-with open(ttlOutput, 'w') as outputFile:
-    outputFile.write('')
-    outputFile.close()
-    
+# Remove existing identifiers from the list
+gndIdentifiersToQuery = [d for d in gndIdentifiers if d not in existingIdentifiers]
+
+# Report on the number of identifiers in the data, already retrieved and yet to retrieve
+print("Total number of GND identifiers in the data: " + str(len(gndIdentifiers)))
+print("Total number of GND identifiers already retrieved: " + str(len(existingIdentifiers)))
+print("Total number of GND identifiers to retrieve: " + str(len(gndIdentifiersToQuery)))
+
 # Retrieve ttl data from GND and append to ttl file
 with open(ttlOutput, 'a') as outputFile:
-    for identifier in tqdm(gndIdentifiers):
+    for identifier in tqdm(gndIdentifiersToQuery):
         url = "%s.ttl" % identifier.replace("https://d-nb.info/gnd/","https://lobid.org/gnd/")
         try:
             with request.urlopen(url) as r:
