a4cf9d2805d42de59e869ddfdb6525e279242012
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-11-04 14:06:34 +0100

    add vocab

--- downloadSources.sh
+++ downloadSources.sh
@@ -71,6 +71,7 @@ download "data/sari/bso-roles-alignment.trig" "data/ttl/additional/bso-roles-ali
 download "data/sari/imageRegions.trig" "data/graphs/imageRegions.trig"
 download "data/sari/titleSimilarities.ttl" "data/graphs/titleSimilarities.ttl"
 download "data/sari/visualSimilarities.ttl" "data/graphs/visualSimilarities.ttl"
+download "data/sari/bso-vocabulary.trig" "data/ttl/additional/bso-vocabulary.trig"
 
 download "data/bso-thesaurus/bso-thesaurus.trig" "data/ttl/additional/bso-thesaurus.trig"
 download "data/additional/aat-events.ttl" "data/ttl/additional/aat-events.ttl"
